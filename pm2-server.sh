13a544398c7490ec7fbbfd885609914655b106ce
Kallef Alexandre <kallef.alexandre@orbital.codes>
2025-01-27 15:22:31 -0300

    chore: add sudo to PM2 start and restart commands

--- pm2-server.sh
+++ pm2-server.sh
@@ -1,2 +1,2 @@
-pm2 start /home/jelastic/ROOT/node_modules/.bin/next -- start ; 
-pm2 restart /home/jelastic/ROOT/node_modules/.bin/next -- start
+sudo pm2 start /home/jelastic/ROOT/node_modules/.bin/next -- start ; 
+sudo pm2 restart /home/jelastic/ROOT/node_modules/.bin/next -- start
