e14f2106a5008f220d89561e80b04059acc6bef3
Kallef Alexandre <kallef.alexandre@orbital.codes>
2025-01-27 15:30:17 -0300

    chore: remove sudo from PM2 start and restart commands

--- pm2-server.sh
+++ pm2-server.sh
@@ -1,2 +1,2 @@
-sudo pm2 start /home/jelastic/ROOT/node_modules/.bin/next -- start ; 
-sudo pm2 restart /home/jelastic/ROOT/node_modules/.bin/next -- start
+pm2 start /home/jelastic/ROOT/node_modules/.bin/next -- start ; 
+pm2 restart /home/jelastic/ROOT/node_modules/.bin/next -- start
